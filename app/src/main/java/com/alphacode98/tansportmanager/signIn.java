91284bf588c6c0cbf7639675f3f79abc99d50de7
yasithWimukthi <it19966922@my.sliit.lk>
2021-10-02 11:18:23 +0530

    implement login

--- app/src/main/java/com/alphacode98/tansportmanager/signIn.java
+++ app/src/main/java/com/alphacode98/tansportmanager/signIn.java
@@ -1,14 +1,122 @@
 package com.alphacode98.tansportmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.sdsmdg.tastytoast.TastyToast;
 
 public class signIn extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private ImageButton signInBtn;
+    private TextView signUpTextView;
+
+    private FirebaseUser user;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_sign_in);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        signInBtn = findViewById(R.id.signInBtn);
+        signUpTextView = findViewById(R.id.signUpTxt);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    TastyToast.makeText(
+                            getApplicationContext(),
+                            "Please enter a valid email !",
+                            TastyToast.LENGTH_LONG,
+                            TastyToast.ERROR
+                    );
+                }else if (TextUtils.isEmpty(password)){
+                    TastyToast.makeText(
+                            getApplicationContext(),
+                            "Please enter your password !",
+                            TastyToast.LENGTH_LONG,
+                            TastyToast.ERROR
+                    );
+                }else if (password.length()<6){
+                    TastyToast.makeText(
+                            getApplicationContext(),
+                            "Password should contain at least 6 characters !",
+                            TastyToast.LENGTH_LONG,
+                            TastyToast.INFO
+                    );
+                }else{
+                    loginWithEmailAndPassword(email, password);
+                }
+            }
+        });
+    }
+
+    private void loginWithEmailAndPassword(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if(e instanceof FirebaseAuthInvalidCredentialsException){
+                            TastyToast.makeText(
+                                    getApplicationContext(),
+                                    "Invalid password. Please try again !",
+                                    TastyToast.LENGTH_LONG,
+                                    TastyToast.ERROR
+                            );
+                        }else if(e instanceof FirebaseAuthInvalidUserException){
+                            TastyToast.makeText(
+                                    getApplicationContext(),
+                                    "Incorrect email address. Please try again !",
+                                    TastyToast.LENGTH_LONG,
+                                    TastyToast.ERROR
+                            );
+                        }else{
+                            TastyToast.makeText(
+                                    getApplicationContext(),
+                                    e.getLocalizedMessage(),
+                                    TastyToast.LENGTH_LONG,
+                                    TastyToast.ERROR
+                            );
+                        }
+                    }
+                });
     }
 }
