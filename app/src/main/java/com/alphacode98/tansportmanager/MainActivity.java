7b7efb15ab229fefb13b6c3dff67a291a7e0825c
ShalithAlahakoon <shalitha97068@gmail.com>
2021-10-03 18:22:50 +0530

    startJourney screen implemented

--- app/src/main/java/com/alphacode98/tansportmanager/MainActivity.java
+++ app/src/main/java/com/alphacode98/tansportmanager/MainActivity.java
@@ -2,13 +2,26 @@ package com.alphacode98.tansportmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton scanQrBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanQrBtn = findViewById(R.id.scanQrBtn);
+
+        scanQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),startJourney.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/alphacode98/tansportmanager/startJourney.java
@@ -0,0 +1,23 @@
+package com.alphacode98.tansportmanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+
+public class startJourney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
