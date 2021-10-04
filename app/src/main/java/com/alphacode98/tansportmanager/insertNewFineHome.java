59a2d90e40fa14ee0cc1946dd21f8c1a931d2abe
ShalithAlahakoon <shalitha97068@gmail.com>
2021-10-04 15:47:24 +0530

    insertNewFineHome Screen Implemented

---
+++ app/src/main/java/com/alphacode98/tansportmanager/insertNewFineHome.java
@@ -0,0 +1,14 @@
+package com.alphacode98.tansportmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class insertNewFineHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_new_fine_home);
+    }
+}
